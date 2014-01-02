8c3b6e5d146ccc9d823b7dd8bd088eaa7e85189b
Kazusa Okuda <kazusa@klamath.jp>
2014-01-02 23:04:42 +0900

    commit

--- fmsynth.c
+++ fmsynth.c
@@ -1,3 +1,14 @@
 #include "fmsynth.h"
 
-int fmstep(fmreg* f){};
+int fmstep(fmreg* f){
+
+    switch(f->fmstatus)
+            case(FMSYNTH_STOP)         : return -1;
+            case(FMSYNTH_ON)             :f->fmstatus = FMSYNTH_ON_ATK;
+            case(FMSYNTH_ON_ATK)    
+
+
+
+
+
+};
